acfb366e887253144cc7ad649dfb91ee8832a6e8
Rubén Triviño Juárez <ruben@willimbu.com>
2020-05-17 19:44:28 +0200

    Updates

--- esm/index.mjs
+++
@@ -1,3 +0,0 @@
-import './index-96f926c6.js';
-import './active-router-b95e9f93.js';
-import './match-path-36fdf5c7.js';
--- types/components/my-googlemap/my-googlemap-service.d.ts
+++
@@ -1,6 +0,0 @@
-declare class MyGoogleMapService {
-    map: any;
-    gMapsInit(mapReference: any): Promise<any>;
-}
-export declare const GoogleMapService: MyGoogleMapService;
-export {};
